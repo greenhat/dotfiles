22d7a92a43d3c5aecb20974adf20747f6453486c
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-08-13 10:28:25 +0300

    fix l-hu for hunk reset;

--- nvim/lua/plugin/gitsigns.lua
+++ nvim/lua/plugin/gitsigns.lua
@@ -18,11 +18,11 @@ require('gitsigns').setup {
 
     ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
     ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['n <leader>hS'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>hu'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
     ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
     ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-	  ['n <leader>hd'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>hd'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
     ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
 
     -- Text objects
