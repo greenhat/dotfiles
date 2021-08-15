3e89a439fe0b2b5f9a7294df20016c2aedbf9939
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-08-15 16:52:34 +0300

    add Tab/S-Tab navigation in autocompletion;

--- nvim/init.lua
+++ nvim/init.lua
@@ -421,6 +421,7 @@ vim.o.completeopt = 'menuone,noinsert'
 -- Compe setup
 require('compe').setup {
   enabled = true,
+  preselect = 'always',
   source = {
     path          = true;
     buffer = {
@@ -442,6 +443,58 @@ require('compe').setup {
   },
 }
 
+-- How to use tab to navigate completion menu?
+-- Tab and S-Tab keys need to be mapped to <C-n> and <C-p> when completion menu is visible. Following example will use
+-- Tab and S-Tab (shift+tab) to navigate completion menu and jump between vim-vsnip placeholders when possible:
+-- from https://github.com/L3MON4D3/LuaSnip
+local function prequire(...)
+local status, lib = pcall(require, ...)
+if (status) then return lib end
+    return nil
+end
+
+local luasnip = prequire('luasnip')
+
+local t = function(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+_G.tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif luasnip and luasnip.expand_or_jumpable() then
+        return t "<Plug>luasnip-expand-or-jump"
+    elseif check_back_space() then
+        return t "<Tab>"
+    else
+        return vim.fn['compe#complete']()
+    end
+end
+_G.s_tab_complete = function()
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif luasnip and luasnip.jumpable(-1) then
+        return t "<Plug>luasnip-jump-prev"
+    else
+        return t "<S-Tab>"
+    end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
+vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
 
 -- Gpush for fugitive
 -- vim.cmd [[ command! -bang -bar -nargs=* Gpush execute 'Dispatch<bang> -dir=' .  fnameescape(FugitiveGitDir()) 'git push' <q-args> ]]
