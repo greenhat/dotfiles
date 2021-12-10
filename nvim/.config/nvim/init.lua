a0adf247a4a7aad539393f6a2f29e02a1761f3f9
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2021-12-10 10:53:30 +0200

    fix nvim warning;

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -207,7 +207,7 @@ require('lualine').setup({
         lualine_y = {
             {
                 'diagnostics',
-                sources = {'nvim_lsp'},
+                sources = {'nvim_diagnostic'},
                 sections = {'error', 'warn', 'info', 'hint'},
                 symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}
             }
