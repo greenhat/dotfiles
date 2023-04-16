47e9213e57186cd901804e3ca3f13f0480fe60fc
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-04-16 22:04:48 +0300

    add grep word under the cursor;

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -16,6 +16,8 @@ return {
     vim.keymap.set('n', '<leader>f', require('fzf-lua').files, { desc = 'Files' })
     vim.keymap.set('n', '<leader>b', require('fzf-lua').buffers, { desc = 'Buffers' })
     vim.keymap.set('n', '<leader>sp', require('fzf-lua').live_grep_glob, { desc = 'Live grep with glob' })
+    vim.keymap.set('n', '<leader>sw', require('fzf-lua').grep_cword, { desc = 'Grep word under cursor' })
+    vim.keymap.set('n', '<leader>sw', require('fzf-lua').grep_cWORD, { desc = 'Grep WORD under cursor' })
     vim.keymap.set('n', '<leader>sh', require('fzf-lua').help_tags, { desc = 'Help tags' })
     vim.keymap.set('n', '<leader>sk', require('fzf-lua').keymaps, { desc = 'Keymaps' })
     -- vim.keymap.set('n', '<leader>c', require('fzf-lua').commands, { desc = 'Commands' })
