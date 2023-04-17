b198239c4d8703cd9cc47c021af2490722d862d8
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-04-17 15:14:36 +0300

    Added keybinding for toggling lsp_lines and add lsp workspace symbols;

--- nvim/.config/nvim/lua/plugins/lsp_lines.lua
+++ nvim/.config/nvim/lua/plugins/lsp_lines.lua
@@ -4,8 +4,11 @@ return {
     require("lsp_lines").setup()
 
     -- Disable virtual_text since it's redundant due to lsp_lines.
-    vim.diagnostic.config({
-      virtual_text = false,
-    })
+    -- vim.diagnostic.config({
+    --   virtual_text = false,
+    -- })
+
+    vim.keymap.set("", "<Leader>ll", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" }
+    )
   end,
 }
