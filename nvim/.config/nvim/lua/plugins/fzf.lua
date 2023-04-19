866397e88df7c921bd2f330c54a454f25c78604e
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-04-19 11:34:28 +0300

    make fzf-lua layout vertical for all pickers;

--- nvim/.config/nvim/lua/plugins/fzf.lua
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -2,6 +2,10 @@ return {
   'ibhagwan/fzf-lua',
   config = function()
     require('fzf-lua').setup {
+      winopts = {
+        preview = {
+          layout = 'vertical' }
+      },
       lsp = {
         -- cwd_only = true,
         includeDeclaration = false,
