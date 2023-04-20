1ff7a063433c0770262578f6852807ec9172d332
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2023-04-20 10:16:36 +0300

    add trouble.nvim;

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -96,12 +96,25 @@ vim.keymap.set('n', '[d',
       {
         severity =
         {
-          min = vim.diagnostic.severity.WARN,
+          min = vim.diagnostic.severity.HINT,
           max = vim.diagnostic.severity.ERROR
         }
       })
   end,
   { desc = "Go to previous diagnostic message" })
+
+vim.keymap.set('n', '[e',
+  function()
+    vim.diagnostic.goto_prev(
+      {
+        severity =
+        {
+          min = vim.diagnostic.severity.ERROR,
+          max = vim.diagnostic.severity.ERROR
+        }
+      })
+  end,
+  { desc = "Go to previous error message" })
 
 vim.keymap.set('n', ']d',
   function()
@@ -109,14 +122,25 @@ vim.keymap.set('n', ']d',
       {
         severity =
         {
-          min = vim.diagnostic.severity.WARN,
+          min = vim.diagnostic.severity.HINT,
           max = vim.diagnostic.severity.ERROR
         }
       })
   end,
   { desc = "Go to next diagnostic message" })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
--- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+vim.keymap.set('n', ']e',
+  function()
+    vim.diagnostic.goto_next(
+      {
+        severity =
+        {
+          min = vim.diagnostic.severity.ERROR,
+          max = vim.diagnostic.severity.ERROR
+        }
+      })
+  end,
+  { desc = "Go to next error message" })
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
@@ -146,30 +170,24 @@ local on_attach = function(client, bufnr)
   end
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  -- nmap('<leader>a', vim.lsp.buf.code_action, '[C]ode [A]ction')
+  nmap('<leader>a', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gy', vim.lsp.buf.type_definition, 'Goto Type [D]efinition')
-  -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  -- nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
   nmap('<leader>j', require('fzf-lua').lsp_live_workspace_symbols, 'Live Workspace Symbols')
   nmap('<leader>J', require('fzf-lua').lsp_workspace_symbols, 'Workspace Symbols')
-  -- nmap('<leader>J', require('fzf-lua').lsp_finder, 'LSP finder')
   nmap('<leader>o', require('fzf-lua').lsp_document_symbols, 'Document Symbols')
   nmap('<leader>a', require('fzf-lua').lsp_code_actions, 'Code Actions')
   vmap('<leader>a', require('fzf-lua').lsp_code_actions, 'Code Actions')
-  -- nmap('gd', require('fzf-lua').lsp_definitions, 'Goto Definition')
   nmap('gD', require('fzf-lua').lsp_declarations, 'Goto Declaration')
-  -- nmap('gy', require('fzf-lua').lsp_typedefs, 'Type Actions')
+  -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('gi', require('fzf-lua').lsp_implementations, 'Goto Implementation')
   nmap('gr', require('fzf-lua').lsp_references, 'Goto References')
-  nmap('<leader>e',
-    function()
-      require('fzf-lua').lsp_workspace_diagnostics({ winopts = { preview = { layout = 'vertical' } } })
-    end,
-    'Workspace Diagnostics')
+  -- nmap('<leader>e',
+  --   function()
+  --     require('fzf-lua').lsp_workspace_diagnostics({ winopts = { preview = { layout = 'vertical' } } })
+  --   end,
+  --   'Workspace Diagnostics')
   nmap('<leader>li', require('fzf-lua').lsp_incoming_calls, 'Incoming Calls')
   nmap('<leader>lo', require('fzf-lua').lsp_outgoing_calls, 'Outgoing Calls')
 
@@ -179,7 +197,6 @@ local on_attach = function(client, bufnr)
   imap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('<leader>wl', function()
@@ -190,6 +207,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
   require('lsp-inlayhints').on_attach(client, bufnr, false)
 end
 
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -26,6 +26,48 @@ return {
       dependencies = {
         'nvim-lua/plenary.nvim',
       }
+    },
+    {
+      "folke/trouble.nvim",
+      dependencies = "nvim-tree/nvim-web-devicons",
+      config = function()
+        require("trouble").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+          -- vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+          --   { silent = true, noremap = true }
+          -- )
+          vim.keymap.set("n", "<leader>e", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+            { silent = true, noremap = true }
+          )
+          -- vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+          --   { silent = true, noremap = true }
+          -- )
+          -- vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+          --   { silent = true, noremap = true }
+          -- )
+          -- vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+          --   { silent = true, noremap = true }
+          -- )
+          -- vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+          --   { silent = true, noremap = true }
+          -- )
+
+        vim.keymap.set('n', '[t',
+          function()
+            require("trouble").previous({skip_groups = true, jump = true});
+          end,
+          { desc = "Go to previous trouble message" })
+
+        vim.keymap.set('n', ']t',
+          function()
+            require("trouble").next({skip_groups = true, jump = true});
+          end,
+          { desc = "Go to next trouble message" })
+
+      end
     }
   },
   config = function()
