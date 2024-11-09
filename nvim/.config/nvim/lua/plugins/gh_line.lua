dd25cdf70e6802a96f3ac4f40d90cd0f47e878d9
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2024-11-09 12:25:06 +0200

    slightly configured lazyvim template

--- nvim/.config/nvim/lua/plugins/gh_line.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'ruanyl/vim-gh-line',
-
-  config = function ()
-    -- Copy link to a clipboard instead of opening a browser:
-    vim.g.gh_open_command = 'fn() { echo "$@" | tee >(wl-copy) >(tmux loadb -); }; fn '
-  end
-}
