52837d744ba8d63fa3780ed78b6358644508fe44
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-09 21:44:04 +0200

    remap telescope keys

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -22,5 +22,14 @@ return {
         -- },
       },
     },
+    keys = {
+      {
+        "<leader><space>",
+        "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",
+        desc = "Switch Buffer",
+      },
+      { "<leader>;", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>e", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+    },
   },
 }
