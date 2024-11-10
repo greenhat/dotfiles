c775f5d17b3fb16b2a1cc864b5025bcb0fe41bed
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-10 05:58:17 +0200

    add winbar

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = {
+    winbar = {
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path() },
+      },
+    },
+    inactive_winbar = {
+      lualine_c = {
+        LazyVim.lualine.root_dir(),
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { LazyVim.lualine.pretty_path() },
+      },
+    },
+  },
+}
