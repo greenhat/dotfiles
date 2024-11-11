064e7b08be48c06860cadc3be5d82aea95741ac9
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-11 07:07:39 +0200

    gruvbox contrast hard

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -7,6 +7,13 @@ return {
         comments = false,
         strings = false,
       },
+      -- inverse = true,
+      contrast = "hard",
+      overrides = {
+        -- LspReferenceText = { fg = "#ebdbb2", bg = "#3c3836" },
+        -- LspReferenceText = { link = "GruvboxOrangeBold" },
+        -- you need to set LspReferenceRead and LspReferenceWrite as well
+      },
     },
   },
 
