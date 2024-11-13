f92f48944bb110bf4a05bba8c79be0b79d0ff5ac
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-13 15:23:00 +0200

    fix flash label color

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,12 +1,17 @@
 return {
   "folke/flash.nvim",
   opts = {
-    labels = "arstneioqwfpluy;",
+    labels = "arstneioqwfpluy;zxcdh,./",
     search = {
       multi_window = false,
       mode = function(str)
         return "\\<" .. str
       end,
+    },
+    highlight = {
+      groups = {
+        label = "FlashCurrent",
+      },
     },
   },
 }
