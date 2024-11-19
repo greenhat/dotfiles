c8aa700730dbd625b3425d954db7152a0143c1d5
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-19 12:55:28 +0200

    hide statusline

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,47 @@
 return {
   "nvim-lualine/lualine.nvim",
-  opts = {
-    winbar = {
-      lualine_c = {
-        LazyVim.lualine.root_dir(),
-        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-        { LazyVim.lualine.pretty_path() },
+  -- enabled = false,
+  -- init = function()
+  --   require("lualine").hide({
+  --     place = { "statusline", "tabline" }, -- The segment this change applies to.
+  --     unhide = false, -- whether to re-enable lualine again/
+  --   })
+  -- end,
+  opts = function()
+    local opts = {
+      sections = {},
+      winbar = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = LazyVim.config.icons.diagnostics.Error,
+              warn = LazyVim.config.icons.diagnostics.Warn,
+              info = LazyVim.config.icons.diagnostics.Info,
+              hint = LazyVim.config.icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_y = {
+          -- { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
       },
-    },
-    inactive_winbar = {
-      lualine_c = {
-        LazyVim.lualine.root_dir(),
-        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-        { LazyVim.lualine.pretty_path() },
+      inactive_winbar = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path() },
+        },
+        lualine_y = {
+          -- { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
       },
-    },
-  },
+    }
+    return opts
+  end,
 }
