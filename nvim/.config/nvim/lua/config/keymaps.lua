e3667faf7efdbc9c2fa9455f6e2f4a825a796cd8
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-23 17:00:49 +0200

    unmap move line up and down

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ local map = vim.keymap.set
 
 map({ "i", "x", "n", "s" }, "<M-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
 map({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- unmap move line up and down
+vim.keymap.del({ "i", "x", "n", "s" }, "<M-j>")
+vim.keymap.del({ "i", "x", "n", "s" }, "<M-k>")
