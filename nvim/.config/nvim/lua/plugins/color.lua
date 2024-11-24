34345caeb79ab1f849f1e38835af53e3afca04b4
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2024-11-24 18:01:39 +0200

    set colorscheme to dayfox

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -28,13 +28,42 @@ return {
       },
     },
   },
+  {
+    "zenbones-theme/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    config = function()
+      vim.g.zenbones_italic_comments = false
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim", -- lazy
+  },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = false
+      vim.g.gruvbox_material_background = "hard"
+    end,
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "gruvbox",
-      colorscheme = "tokyonight",
+      --
+      -- colorscheme = "gruvbox-material",
+      --
+      -- colorscheme = "tokyonight",
+      --
+      -- damn italics for types and constants
+      -- colorscheme = "zenbones",
+      --
+      colorscheme = "dayfox",
     },
   },
 }
