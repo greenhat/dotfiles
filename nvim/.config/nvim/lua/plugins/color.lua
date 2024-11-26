5809a676a8898b4e94f60d19f7dfd8196e10243e
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2024-11-26 05:48:39 +0200

    removed unused colorschemes

--- nvim/.config/nvim/lua/plugins/color.lua
+++ nvim/.config/nvim/lua/plugins/color.lua
@@ -1,56 +1,54 @@
 return {
-  -- add gruvbox
-  {
-    "ellisonleao/gruvbox.nvim",
-    opts = {
-      italic = {
-        comments = false,
-        strings = false,
-      },
-      -- inverse = true,
-      contrast = "hard",
-      overrides = {
-        -- LspReferenceText = { fg = "#ebdbb2", bg = "#3c3836" },
-        -- LspReferenceText = { link = "GruvboxOrangeBold" },
-        -- you need to set LspReferenceRead and LspReferenceWrite
-      },
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "day",
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = {},
-        variables = {},
-      },
-    },
-  },
-  {
-    "zenbones-theme/zenbones.nvim",
-    dependencies = "rktjmp/lush.nvim",
-    config = function()
-      vim.g.zenbones_italic_comments = false
-    end,
-  },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   opts = {
+  --     italic = {
+  --       comments = false,
+  --       strings = false,
+  --     },
+  --     -- inverse = true,
+  --     contrast = "hard",
+  --     overrides = {
+  --       -- LspReferenceText = { fg = "#ebdbb2", bg = "#3c3836" },
+  --       -- LspReferenceText = { link = "GruvboxOrangeBold" },
+  --       -- you need to set LspReferenceRead and LspReferenceWrite
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     style = "day",
+  --     styles = {
+  --       comments = { italic = false },
+  --       keywords = { italic = false },
+  --       functions = {},
+  --       variables = {},
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "zenbones-theme/zenbones.nvim",
+  --   dependencies = "rktjmp/lush.nvim",
+  --   config = function()
+  --     vim.g.zenbones_italic_comments = false
+  --   end,
+  -- },
   {
     "EdenEast/nightfox.nvim", -- lazy
   },
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_enable_italic = false
-      vim.g.gruvbox_material_background = "hard"
-    end,
-  },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     -- Optionally configure and load the colorscheme
+  --     -- directly inside the plugin declaration.
+  --     vim.g.gruvbox_material_enable_italic = false
+  --     vim.g.gruvbox_material_background = "hard"
+  --   end,
+  -- },
 
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
