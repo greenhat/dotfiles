96225840ac2b9fa8657198c59ec1d8ed04f58726
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2024-12-18 06:35:33 +0200

    add hardtime.nvim

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    command = "Hardtime",
+    event = "BufEnter",
+    keys = {
+      { "n", "j", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+      { "n", "k", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+      { "n", "gj", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+      { "n", "gk", "<cmd>Hardtime<CR>", desc = "Hardtime" },
+    },
+  },
+}
