5ed7ffa1d96815c99d8a8a6d3e8ccf9ecec93fec
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2025-01-26 11:37:32 +0200

    disable diffview.nvim integration in neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -2,8 +2,7 @@ return {
   "NeogitOrg/neogit",
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
-
+    -- "sindrets/diffview.nvim", -- optional - Diff integration
     -- Only one of these is needed.
     -- "nvim-telescope/telescope.nvim", -- optional
     -- "ibhagwan/fzf-lua", -- optional
