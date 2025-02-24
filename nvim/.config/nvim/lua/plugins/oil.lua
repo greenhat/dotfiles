5a260d3933d37f7f0160846f335957c16e8afd5b
Denys Zadorozhnyi <dzadorozhnyi@polygon.technology>
2025-02-24 13:35:03 +0200

    set Oil shortcut

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -6,4 +6,11 @@ return {
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  keys = {
+    {
+      "<leader>fo",
+      "<cmd>Oil<CR>",
+      desc = "Oil",
+    },
+  },
 }
