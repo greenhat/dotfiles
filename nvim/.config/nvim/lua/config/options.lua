60a1f6fa64a20b604efb79848bd6475068dfc17f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-03-23 15:59:40 +0200

    enable yanky.nvim, paste without osc 52

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -49,7 +49,29 @@ vim.opt.background = "light"
 
 vim.opt.autoread = true
 
--- always use OSC 52 (otherwise will be wl-copy on wayland)
+-- -- always use OSC 52 (otherwise will be wl-copy on wayland)
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--   },
+--   paste = {
+--     ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+--     ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+--   },
+-- }
+
+-- -- https://github.com/neovim/neovim/discussions/28010
+vim.o.clipboard = "unnamedplus"
+
+local function paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
 vim.g.clipboard = {
   name = "OSC 52",
   copy = {
@@ -57,7 +79,7 @@ vim.g.clipboard = {
     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
   },
   paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    ["+"] = paste,
+    ["*"] = paste,
   },
 }
