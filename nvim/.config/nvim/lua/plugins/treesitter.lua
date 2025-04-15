c142c53ece6b983306d19e88e3a1ea99bb815faa
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-04-15 19:06:12 +0300

    add wit ts config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  keys = {
-    { "L", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    keys = {
+      { "L", desc = "Increment Selection" },
+      { "<bs>", desc = "Decrement Selection", mode = "x" },
+    },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      ensure_installed = {
+        "wit",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "L",
+          node_incremental = "L",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
   },
-  ---@type TSConfig
-  ---@diagnostic disable-next-line: missing-fields
-  opts = {
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "L",
-        node_incremental = "L",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+  {
+    "folke/ts-comments.nvim",
+    opts = {
+      lang = {
+        wit = "// %s",
       },
     },
   },
