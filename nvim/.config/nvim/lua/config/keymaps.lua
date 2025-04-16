435418f6e2db79d1b55569b63e8692276a10534f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-04-16 07:00:31 +0300

    enable nvim-tmux-navigator with CMD mapping

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -22,8 +22,8 @@ end, { desc = "Git Browse (copy)" })
 -- readline like keymaps on macos
 map({ "i" }, "<M-BS>", "<C-w>", { desc = "delete word backward" })
 
--- Move to window using the <cmd> hjkl keys
-map("n", "<M-h>", "<C-w>h", { desc = "Go to Left Window" })
-map("n", "<M-j>", "<C-w>j", { desc = "Go to Lower Window" })
-map("n", "<M-k>", "<C-w>k", { desc = "Go to Upper Window" })
-map("n", "<M-l>", "<C-w>l", { desc = "Go to Right Window" })
+-- -- Move to window using the <cmd> hjkl keys
+-- map("n", "<M-h>", "<C-w>h", { desc = "Go to Left Window" })
+-- map("n", "<M-j>", "<C-w>j", { desc = "Go to Lower Window" })
+-- map("n", "<M-k>", "<C-w>k", { desc = "Go to Upper Window" })
+-- map("n", "<M-l>", "<C-w>l", { desc = "Go to Right Window" })
--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -41,4 +41,55 @@ return {
     { "noice.nvim", cond = not vim.g.started_by_firenvim },
     { "lualine.nvim", cond = not vim.g.started_by_firenvim }, -- not useful in the browser
   },
+  {
+    "alexghergh/nvim-tmux-navigation",
+    lazy = false,
+    -- config = function()
+    --   local nvim_tmux_nav = require("nvim-tmux-navigation")
+    --
+    --   nvim_tmux_nav.setup({
+    --     disable_when_zoomed = true, -- defaults to false
+    --   })
+    --
+    --   vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+    --   vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+    --   vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+    --   vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    --   vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+    --   vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+    -- end,
+    opts = {
+      disable_when_zoomed = true,
+    },
+    keys = {
+      {
+        "<M-h>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
+        end,
+        desc = "tmux nav left",
+      },
+      {
+        "<M-j>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateDown()
+        end,
+        desc = "tmux nav down",
+      },
+      {
+        "<M-k>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateUp()
+        end,
+        desc = "tmux nav up",
+      },
+      {
+        "<M-l>",
+        function()
+          require("nvim-tmux-navigation").NvimTmuxNavigateRight()
+        end,
+        desc = "tmux nav right",
+      },
+    },
+  },
 }
