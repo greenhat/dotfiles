685a86bfd6a8b792ce649825632e8ff02091d063
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-04-24 11:25:43 +0300

    map RustLsp renderDiagnostic

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -4,9 +4,9 @@ return {
     opts = {
       server = {
         on_attach = function(_, bufnr)
-          -- vim.keymap.set("n", "<leader>cR", function()
-          --   vim.cmd.RustLsp("codeAction")
-          -- end, { desc = "Code Action", buffer = bufnr })
+          vim.keymap.set("n", "<C-e>", function()
+            vim.cmd.RustLsp("renderDiagnostic")
+          end, { desc = "Render diagnostic", buffer = bufnr })
           -- vim.keymap.set("n", "<leader>dr", function()
           --   vim.cmd.RustLsp("debuggables")
           -- end, { desc = "Rust Debuggables", buffer = bufnr })
