fa2479f79007d21d95ea865efdfa8b7535707d08
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-06-01 16:10:38 +0300

    add neogit

---
+++ nvim-kickstart/.config/nvim-kickstart/lua/custom/plugins/neogit.lua
@@ -0,0 +1,24 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- required
+    -- "sindrets/diffview.nvim", -- optional - Diff integration
+    -- Only one of these is needed.
+    -- "nvim-telescope/telescope.nvim", -- optional
+    -- "ibhagwan/fzf-lua", -- optional
+    -- "echasnovski/mini.pick", -- optional
+  },
+  config = true,
+  opts = {
+    commit_editor = {
+      staged_diff_split_kind = "vsplit",
+    },
+  },
+  keys = {
+    {
+      "<leader>gn",
+      "<cmd>wa | Neogit<CR>",
+      desc = "Neogit",
+    },
+  },
+}
