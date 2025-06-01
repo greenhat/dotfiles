518f5bab50e270eb33a65d26ec1a5e7a1935a691
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-06-01 16:08:24 +0300

    add close buffer and close other buffers

--- nvim-kickstart/.config/nvim-kickstart/lua/keymaps.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/keymaps.lua
@@ -89,4 +89,7 @@ map('n', '<Leader>qq', '<cmd>qa<cr><esc>', { desc = 'Quit' })
 
 map('n', '<Leader><tab>s', '<cmd>tab split<cr>', { desc = 'open in new tab' })
 
+map('n', '<Leader>bd', '<cmd>bprevious <bar> bdelete #<CR>', { desc = 'close buffer' })
+map('n', '<Leader>bd', '<cmd>%bd|e#<CR>', { desc = 'close all but the current buffer' })
+
 -- vim: ts=2 sts=2 sw=2 et
