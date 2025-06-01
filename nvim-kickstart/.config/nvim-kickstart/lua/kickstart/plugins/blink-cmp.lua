08c5239f2f1cadb84971ae8bc81e250d2614c548
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2025-06-01 13:16:09 +0300

    tune blink.cmp

--- nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/blink-cmp.lua
+++ nvim-kickstart/.config/nvim-kickstart/lua/kickstart/plugins/blink-cmp.lua
@@ -61,6 +61,21 @@ return {
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+
+        ['<C-e>'] = { 'show', 'show_documentation', 'hide_documentation' },
+
+        -- accept word
+        ['<M-Tab>'] = {
+          function()
+            if require('copilot.suggestion').is_visible() then
+              LazyVim.create_undo()
+              require('copilot.suggestion').accept_word()
+              return true
+            end
+          end,
+          'snippet_forward',
+          'fallback',
+        },
       },
 
       appearance = {
@@ -72,7 +87,10 @@ return {
       completion = {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+        documentation = { auto_show = true, auto_show_delay_ms = 200 },
+        menu = {
+          auto_show = false,
+        },
       },
 
       sources = {
