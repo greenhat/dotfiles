771d214084e45a4dc928527d1e712ddc8dc2443b
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-01-19 15:53:43 +0200

    remove copilot

--- nvim/.config/nvim/lua/custom/plugins/copilot.lua
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -1,11 +1,12 @@
-return {
-  'zbirenbaum/copilot.lua',
-  opts = {
-    suggestion = {
-      auto_trigger = false,
-      keymap = {
-        accept = '<tab>',
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   'zbirenbaum/copilot.lua',
+--   opts = {
+--     suggestion = {
+--       auto_trigger = false,
+--       keymap = {
+--         accept = '<tab>',
+--       },
+--     },
+--   },
+-- }
