21a3adbb404a5ff902c1647706a5ff94f352ad5e
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2026-04-03 11:15:54 +0300

    incremental selection

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -5,7 +5,23 @@ return {
     build = ':TSUpdate',
 
     config = function()
-      require('nvim-treesitter').install { 'rust' }
+      -- require('nvim-treesitter').install { 'rust' }
+      -- incremental selection treesitter/lsp
+      vim.keymap.set({ 'n', 'x', 'o' }, 'L', function()
+        if vim.treesitter.get_parser(nil, nil, { error = false }) then
+          require('vim.treesitter._select').select_parent(vim.v.count1)
+        else
+          vim.lsp.buf.selection_range(vim.v.count1)
+        end
+      end, { desc = 'Select parent treesitter node or outer incremental lsp selections' })
+
+      vim.keymap.set({ 'n', 'x', 'o' }, 'H', function()
+        if vim.treesitter.get_parser(nil, nil, { error = false }) then
+          require('vim.treesitter._select').select_child(vim.v.count1)
+        else
+          vim.lsp.buf.selection_range(-vim.v.count1)
+        end
+      end, { desc = 'Select child treesitter node or inner incremental lsp selections' })
     end,
   },
   -- -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
